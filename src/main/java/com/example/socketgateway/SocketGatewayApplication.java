3b11d20afce4858b7b1d332959eb37e071ea86b7
vvoronkov <vvoronkov@zuzex.com>
2023-02-28 13:52:01 +0300

    Initial commit

---
+++ src/main/java/com/example/socketgateway/SocketGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.example.socketgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocketGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocketGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/socketgateway/SocketGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.socketgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocketGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
